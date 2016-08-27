2e1ed3d7fb8fdc6d1eeaa4fd8c8ef661a3242a7e
psjaat <pankajsaharan@gmail.com>
2016-08-27 19:42:08 +0530

    changes

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// declare modules
+angular.module('Authentication', []);
+angular.module('Home', []);
+
+angular.module('BasicHttpAuthExample', [
+    'Authentication',
+    'Home',
+    'ngRoute',
+    'ngCookies'
+])
+
+.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/mycredit', {
+            controller: 'LoginController',
+            templateUrl: 'modules/authentication/views/login.html',
+            hideMenus: true
+        })
+ 
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'modules/home/views/home.html'
+        })
+		
+		.when('/register', {
+            controller: 'LoginController',
+            templateUrl: 'modules/home/views/register.html'
+        })
+ 
+        .otherwise({ redirectTo: '/mycredit' });
+}])
+ 
+.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+			
+        $rootScope.globals = $cookieStore.get('globals') || {};
+		
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+ 
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if (!$rootScope.globals.currentUser) {
+                
+            }
+        });
+    }]);
+	
